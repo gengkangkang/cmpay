3c577f5f9beb4635a9f6399bef003799ab2bdd9a
gkk <tjeagle@163.com>
2016-11-15 14:57:58 +0800

    微信支付模块

---
+++ cmpay/pay-service-weixin/src/main/java/com/cmpay/service/weixin/dao/CmpayRecordMapper.java
@@ -0,0 +1,30 @@
+package com.cmpay.service.weixin.dao;
+
+import com.cmpay.service.weixin.model.CmpayRecord;
+import com.cmpay.service.weixin.model.CmpayRecordExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface CmpayRecordMapper {
+    int countByExample(CmpayRecordExample example);
+
+    int deleteByExample(CmpayRecordExample example);
+
+    int deleteByPrimaryKey(String id);
+
+    int insert(CmpayRecord record);
+
+    int insertSelective(CmpayRecord record);
+
+    List<CmpayRecord> selectByExample(CmpayRecordExample example);
+
+    CmpayRecord selectByPrimaryKey(String id);
+
+    int updateByExampleSelective(@Param("record") CmpayRecord record, @Param("example") CmpayRecordExample example);
+
+    int updateByExample(@Param("record") CmpayRecord record, @Param("example") CmpayRecordExample example);
+
+    int updateByPrimaryKeySelective(CmpayRecord record);
+
+    int updateByPrimaryKey(CmpayRecord record);
+}
